d85b2b5dc5e86c59f97743729e2b5742fe01e313
fabian123z3 <fabianvillablanca97@gmail.com>
2023-11-19 18:17:18 -0300

    1.3

--- app/src/main/java/cl/santotomas/fabianplants/alarmasmain.java
+++ app/src/main/java/cl/santotomas/fabianplants/alarmasmain.java
@@ -13,8 +13,6 @@ import android.widget.TimePicker;
 public class alarmasmain extends AppCompatActivity {
     ImageButton habitos;
     ImageButton trofeos;
-    private int hour, minute;
-    private Button selectTimeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +21,6 @@ public class alarmasmain extends AppCompatActivity {
 
         habitos = findViewById(R.id.Habitos);
         trofeos = findViewById(R.id.Trofeo);
-        selectTimeButton = findViewById(R.id.selectTimeButton);
 
         habitos.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,27 +38,5 @@ public class alarmasmain extends AppCompatActivity {
             }
         });
 
-        selectTimeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showTimePickerDialog();
-            }
-        });
-    }
-
-    public void showTimePickerDialog() {
-        TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
-            @Override
-            public void onTimeSet(TimePicker view, int selectedHour, int selectedMinute) {
-                // La hora y el minuto seleccionados por el usuario
-                hour = selectedHour;
-                minute = selectedMinute;
-
-                // Aquí puedes configurar la lógica para programar las notificaciones en la hora seleccionada.
-                // Puedes usar AlarmManager y BroadcastReceiver para manejar las notificaciones.
-                // Consulta la documentación de Android para obtener más detalles.
-            }
-        }, hour, minute, true);
-        timePickerDialog.show();
     }
 }
